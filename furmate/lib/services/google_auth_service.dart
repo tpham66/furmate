3d5eede1bc4c8aea3262b9da7d9083161d0fb70f
Thuy Duong Pham <132012852+tpham66@users.noreply.github.com>
2024-10-12 13:00:00 -0500

    add google_auth_service.dart to services folder to handle sign in with google

---
+++ furmate/lib/services/google_auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthService {
+  String errorMessage = '';
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      // Trigger the Google sign-in flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return null; // The user cancelled sign in
+
+      // Obtain the authentication details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Create a credential using the Google authentication details
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Sign in to Firebase using the Google credential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      errorMessage = 'Error signing in with Google: $e';
+      return null;
+    }
+  }
+}
